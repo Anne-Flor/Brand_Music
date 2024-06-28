8a397efece0d152ef392ffdbf134df07309bdf79
Flor <annygabrielly730@gmail.com>
2024-06-27 21:46:42 -0300

    Primeiro Commit

---
+++ Modelos/Audio.java
@@ -0,0 +1,41 @@
+package Modelos;
+
+public class Audio{
+    private double duration; 
+    private String title;
+    private int totalReproduction; 
+    private int totalLikes; 
+    private double classification;
+    
+    public double getDuration(){
+        return duration; 
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public void setTitle(String title){
+        this.title = title; 
+    }
+    
+    public int getTotalReproduction(){
+        return totalReproduction; 
+    }
+
+    public int getTotalLikes(){
+        return totalLikes; 
+    }
+
+    public double getClassification(){
+        return classification; 
+    }
+
+    public void likes(){
+        this.totalLikes++;
+    }
+
+    public void reproduce(){
+        this.totalReproduction++;
+    }
+}
